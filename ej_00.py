4cc2c9485833678e31854b8ebdaed994b2df4b14
Hector Costa <hcostaguzman@gmail.com>
2018-05-24 16:25:50 +0200

    Create ej_00.py

---
+++ ej_00.py
@@ -0,0 +1,9 @@
+import turtle as t       # Cargamos el módulo de la tortuga
+
+t.setup(500,500)         # Configuramos un espacio de dibujo
+
+t.shape("turtle")        # Le damos la forma de una tortuga 
+t.color("green")         # Y un bonito color verde
+
+t.done()                 # Igualmente debemos poner abajo del todo
+t.bye()                  # Un done-bye para cerrar las rutinas
